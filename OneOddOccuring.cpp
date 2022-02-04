e52f2f39a115d7151303f82842235f43f1b05e54
Chhaya7817 <chhaya.chaudhary.visitme.in91@gmail.com>
2022-02-04 23:04:37 +0530

    First DSA Commit

---
+++ OneOddOccuring.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main()
+{
+    int n,result;
+    std::cout<<"Enter Size of array: ";
+    std::cin>>n;
+    int arr[n];
+    std::cout<<"Enter Elements in array\n NOTE: only one element occur odd times....";
+
+    for(int i=0;i<n;i++)
+    {
+        std::cin>>arr[i];
+    }
+    result=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        result=result ^arr[i];
+    }
+    std::cout<<"Number Occuring Odd times = "<<result;
+    return 0;
+}
